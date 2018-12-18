d90b0f9da240dedb3975bf229c6a29e8a46b2435
Gustavo Bazan <gustavo@swiftcomply.com>
2018-12-18 10:20:10 +0000

    Relax countries gem dependency
    Remove support for EOL ruby and rails version

--- lib/country_select.rb
+++ lib/country_select.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 require 'countries'
 require 'sort_alphabetical'
 
@@ -7,8 +5,4 @@ require 'country_select/version'
 require 'country_select/formats'
 require 'country_select/tag_helper'
 
-if defined?(ActionView::Helpers::Tags::Base)
-  require 'country_select/country_select_helper'
-else
-  require 'country_select/rails3/country_select_helper'
-end
+require 'country_select/country_select_helper'
